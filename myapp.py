ca1e8a3da8e80bc4974cdc17786b6526d6e8511b
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-01-09 12:24:38 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,25 @@
+# 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
+
+import openai
+
+openai.api_key = "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"
+
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message.strip()
+
+
+while True:
+    prompt = input("You: ")
+    response = generate_response(prompt)
+    print("Terminal_bot: ", response)
